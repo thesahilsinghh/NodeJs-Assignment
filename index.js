bf76759db382164a90045a20bb91581619316c2a
Sahil Singh <thesahilsinghh@gmail.com>
2024-05-26 16:27:16 +0530

    error handling

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use(express.json());
 
 //route
 app.use("/app/form/", userRouter);
+
+app.use("", (err, req, res, next) => {
+  res.status(503).send("Server is facing an issue! please try again later.");
+});
 //exporting to server
 export default app;
